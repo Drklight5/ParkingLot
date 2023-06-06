85c2ed0a8defc70591f68398d7d6eb7e078b4ff7
Valeria <valeria050603@hotmail.com>
2023-06-06 10:22:46 -0600

    Finish methods

--- Time.h
+++ Time.h
@@ -15,10 +15,10 @@ class Time{
 
         //Getters and setters
         int getHour(){return hour;}
-        void setHour(int h){hour = h;}
+        void setHour(int h);
         
         int getMinute(){return minute;}
-        void setMinute(int m){minute = m;}
+        void setMinute(int m);
 
         //Methods
         int elapsedTime(Time);
@@ -28,27 +28,75 @@ class Time{
 };
 
 Time::Time(){
-    hour = 7; 
-    minute = 30;
+    setHour(0); 
+    setMinute(0);
 }
 
 Time::Time(int h, int m){
-    hour = h;
-    minute = m;
+    setHour(h);
+    setMinute(m);
 }
 
+void Time::setHour(int h){
+
+    if (h <= 22 && h >= 8){
+        hour = h;
+    }
+}
+
+void Time::setMinute(int m){
+    if (hour == 22 && m == 0){
+        minute = m;
+    }
+    else if (m <= 59 && m >= 0){
+        minute = m;
+    }
+}
 int Time::elapsedTime(Time time){
-    //TODO
-    return 0;
+    //
+    int m1,m2, result;
+    if (*this > time){
+        m2 = hour * 60 + minute;
+        m1 = time.getHour() * 60 + minute;  
+    }
+    else{
+        m1 = hour * 60 + minute;
+        m2 = time.getHour() * 60 + minute;  
+    }
+
+    result = (m2 - m1) / 60; 
+    return int(result);
 }
 
-bool Time::operator>(Time time){
+bool Time::operator >(Time time){
     //TODO
-    return false;
+    int hour2 = time.getHour();
+    if ( hour > hour2){
+        return true;
+    }
+    else if (hour2 == hour){
+        if (minute > time.getMinute()){
+            return true;
+        }
+        else {
+            return false;
+        }    
+    }  
+    else{
+        return false;
+    }
+    //TODO it can be shorter 
 }
 
 void Time::print(){
-    cout << "Son las: " << hour << ":" << minute<< endl;
+    cout  << hour << ":";
+    if (minute < 10){
+        cout << "0" << minute<< endl;
+    }else{
+        cout << minute<< endl;
+    }
+    //TODO it can be shorter
+    
 }
 
 
