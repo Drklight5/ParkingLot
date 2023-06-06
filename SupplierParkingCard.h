05f52b26a4635ace3c24080398b52934a0dcb6f9
Valeria <valeria050603@hotmail.com>
2023-06-06 09:45:22 -0600

    Finish methods

--- SupplierParkingCard.h
+++ SupplierParkingCard.h
@@ -24,20 +24,24 @@ public:
 };
 
 SupplierParkingCard::SupplierParkingCard():ParkingCard(){
-    // Esto es para tenerlo listo la definicion se hara posteriormente 
+    company = "";
+        // Esto es para tenerlo listo la definicion se hara posteriormente 
 }
 
 SupplierParkingCard::SupplierParkingCard(int id, string name, string com):ParkingCard(id, name){
+    company = com;
     // Esto es para tenerlo listo la definicion se hara posteriormente 
 
 }
 
-int SupplierParkingCard::calculateHoursToPay(Time, Time){
+int SupplierParkingCard::calculateHoursToPay(Time start, Time end){
+    int hours = start.elapsedTime(end);
     // Este return 0 es solamente para que no me marque error la definicion correcta se hara posteriormente 
-    return 0;
+    return hours - 1;
 }
 
 void SupplierParkingCard::print(){
+    cout << "CardId: " << cardId << "\t Name: " << personName << "\t Company: " << company << endl;
     // La definicion de hara posteriormente 
 }
 #endif
