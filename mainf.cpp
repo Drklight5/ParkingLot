65653a566fd7cf59156f6796536ed469e2c9f1fa
fcruzl44 <ferelizondogm@gmail.com>
2023-06-07 09:27:08 -0600

    Comienzos del main

---
+++ mainf.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "CustomerParkingCard.h"
+#include "EmployeeParkingCard.h"
+#include "SupplierParkingCard.h"
+#include "Time.h"
+#include "ParkingRegister.h"
+
+void readFromFile(ParkingCard cards[50],  )
+
+int main(){
+
+}
