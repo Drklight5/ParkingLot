4ec0df8af561c086f1ab2bbb56b181b6396793e8
fcruzl44 <ferelizondogm@gmail.com>
2023-06-07 10:52:28 -0600

    Continuacion de main

--- mainf.cpp
+++ mainf.cpp
@@ -9,7 +9,7 @@ using namespace std;
 #include "Time.h"
 #include "ParkingRegister.h"
 
-void readFromFile(ParkingCard cards[50], int &numCards ){
+void readFromFile(ParkingCard cards[50], int &numSlots ){
     char card;
     int id;
     string name, store, company;
@@ -18,41 +18,41 @@ void readFromFile(ParkingCard cards[50], int &numCards ){
     inputFile.open("Parkingcards.txt");
     numCards = 0;   
 
-    while(inputFile >> card) {
+    while(inputFile >> card && numSlots <= 20) {
         inputFile >> id >> name >> store >> company ;
         if (card == 'E') {
-            cards[numCards] = new EmployeeParkingCard(id, name, store);
-            numCards++;   // count the person added to the array
+            cards[numSlots] = new EmployeeParkingCard(id, name, store);
+            numSlots++;   
         }
         else if (card == 'S'){
-            cards[numCards] = new SupplierParkingCard(id, name, company);
-            numCards++;   // count the person added to the array
+            cards[numSlots] = new SupplierParkingCard(id, name, company);
+            numSlots++;   // count the person added to the array
+        }
+        else if (card == 'C'){
+            cards[numSlots] = new SupplierParkingCard(id, name);
+            numSlots++;   
         }
     }
     inputFile.close();
 }
 }
 
-void readInfoFromKeyboard(ParkingCard *cards[50], int &numCards) {
+void readInfoFromKeyboard(ParkingCard *cards[50], int &numSlots) {
     char cardChoice;
     int id;
     string name, store, company;
-    cout << "Number of persons? ";
-    cin >> numberOfPersons;
-    for (int c = 0; c < numberOfPersons; c++) {
-        cout << "Is the person a teacher? (y/n)" << endl;
-        cin >> personChoice;
-        cout << "Name? ";
-        cin >> name;
-        cout << "Birth Date? type dd mm yy ";
-        cin >> dd >> mm >> yy;
-        Date date(dd,mm,yy);
-        if (personChoice == 'y') {
+    cout << "Number of Slots ";
+    cin >> numSlots;
+    for (int c = 0; c < numSlots; c++) {
+        cout << "Is the person a Customer? (y/n)" << endl;
+        cin >> cardChoice;
+        if (cardChoice == 'y') {
             cout << "Department? ";
             cin >> depart;
             Teacher *teach = new Teacher(name, date, depart);
             personsList[c] = teach;
         }
+        
         else {
             personsList[c] = new Person(name, date);
         }
